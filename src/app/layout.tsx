5510229dada0eabf48535a9ba8ca90b57fec20ad
Jason Libbey <jelgt2011@gmail.com>
2025-12-26 11:37:43 -0800

    Add @vercel/analytics package and integrate Analytics component into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
 import { StockfishProvider } from "@/contexts/StockfishContext";
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { JetBrains_Mono, Space_Grotesk } from "next/font/google";
 import "./globals.css";
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${spaceGrotesk.variable} ${jetbrainsMono.variable} antialiased flex flex-col min-h-screen`}
       >
+        <Analytics />
         <Navbar />
         <StockfishProvider>
           <main className="flex-1">
